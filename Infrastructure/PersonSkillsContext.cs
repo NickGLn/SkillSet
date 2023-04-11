c56ca6de8a8a2170f11f9e9b148a231a59a2cb83
NickGLn <nikgutorof@gmail.com>
2023-04-11 17:49:30 +0300

    Added DbContext for database configuration

---
+++ Infrastructure/PersonSkillsContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using SkillSet.Domain;
+using Microsoft.Extensions.Hosting;
+using System.Reflection.Metadata;
+using Microsoft.Extensions.Configuration;
+
+namespace SkillSet.Infrastructure;
+
+public class PersonSkillsContext : DbContext
+{
+    public DbSet<Person> People { get; set; }
+    public DbSet<Skill> Skills { get; set; }
+    public DbSet<SkillHistory> SkillsHistory { get; set; }
+
+    protected readonly IConfiguration _configuration;
+
+    public PersonSkillsContext (IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlServer(_configuration.GetConnectionString("PersonSkillsDatabase"));
+}

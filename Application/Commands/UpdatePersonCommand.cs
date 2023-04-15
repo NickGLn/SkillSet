39ddb88f62d1f34516a148a51d0265285c2ba4a0
NickGLn <nikgutorof@gmail.com>
2023-04-15 03:28:23 +0300

    Added UpdatePerson api method

---
+++ Application/Commands/UpdatePersonCommand.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SkillSet.Application.Models;
+using SkillSet.Domain;
+using SkillSet.Infrastructure;
+
+namespace SkillSet.Application.Commands
+{
+    public class UpdatePersonCommand : IRequest<long>
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+        public IEnumerable<SkillDto> Skills { get; set; }
+    }
+
+    public class UpdatePersonCommandHandler : IRequestHandler<UpdatePersonCommand, long>
+    {
+        private readonly IDbContextFactory<PersonSkillsContext> _contextFactory;
+        private readonly IMapper _mapper;
+
+        public UpdatePersonCommandHandler(IDbContextFactory<PersonSkillsContext> contextFactory, IMapper mapper)
+        {
+            _contextFactory = contextFactory;
+            _mapper = mapper;
+        }
+
+        public async Task<long> Handle(UpdatePersonCommand request, CancellationToken cancellationToken)
+        {
+            using var context = _contextFactory.CreateDbContext();
+            var person = await context.People.Include(p => p.Skills)
+                                             .Where(p => p.Id == request.Id)
+                                             .FirstOrDefaultAsync();
+
+            if (person == null)
+            {
+                return 0;
+            }
+
+            person.Name = request.Name;
+            person.DisplayName = request.DisplayName;
+            person.Skills = request.Skills.Select( s => _mapper.Map<Skill>(s)).ToList();
+
+            await context.SaveChangesAsync(cancellationToken);
+
+            return person.Id;
+        }
+    }
+}

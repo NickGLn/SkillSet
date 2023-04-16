b0aabdde0bcccad37729e477c12bf471dfdef443
NickGLn <nikgutorof@gmail.com>
2023-04-17 00:29:33 +0300

    Applied configuration on db context

--- Infrastructure/PersonSkillsContext.cs
+++ Infrastructure/PersonSkillsContext.cs
@@ -1,22 +1,29 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SkillSet.Domain;
 using MediatR;
+using System.Reflection;
 
 namespace SkillSet.Infrastructure;
 
 public class PersonSkillsContext : DbContext
 {
     private readonly IMediator _mediator;
-
     public PersonSkillsContext(
         DbContextOptions<PersonSkillsContext> options,
         IMediator mediator)
-        :base(options)
+        : base(options)
     {
         _mediator = mediator;
     }
     public DbSet<Person> People { get; set; }
     public DbSet<Skill> Skills { get; set; }
     public DbSet<SkillHistory> SkillsHistory { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        base.OnModelCreating(builder);
+    }
 
 }
